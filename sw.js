a54b40d616a1b502a7d5efa3fbf6d2a9c9aec765
langzonedev <greg.lang35@gmail.com>
2026-08-14 16:03:34 +0930

    Refresh TimeStem web cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='timestem-web-v2';
+const CACHE='timestem-web-v3';
 const ASSETS=['./','./index.html','./styles.css','./ux.css','./app.js','./ux.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
