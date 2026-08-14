17f6bce72eac6ba8254ceba7f7b48bd6b91f949e
langzonedev <greg.lang35@gmail.com>
2026-08-14 12:31:16 +0930

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='timestem-web-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
