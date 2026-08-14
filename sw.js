d10d1b4936ff8577c9b0af9283f0ccc9d0248823
langzonedev <greg.lang35@gmail.com>
2026-08-14 17:27:12 +0930

    Refresh TimeStem PWA cache strategy

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE='timestem-web-v4';
+const CACHE='timestem-web-v5';
 const ASSETS=['./','./index.html','./styles.css','./ux.css','./app.js','./ux.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
-  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
-    const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;
-  }).catch(()=>caches.match('./index.html'))));
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
 });
