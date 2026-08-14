2a014edfbbd61c8836f7baa5dbf642cd454bf45e
langzonedev <greg.lang35@gmail.com>
2026-08-14 14:18:35 +0930

    Refresh PWA cache for UX update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='timestem-web-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='timestem-web-v2';
+const ASSETS=['./','./index.html','./styles.css','./ux.css','./app.js','./ux.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>{
